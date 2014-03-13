3e81d557e7678311f87f6c25d2e1b9be7e098616
Code Honey <codehoney@users.noreply.github.com>
2014-03-13 09:53:56 -0700

    Create ex2.py

---
+++ ex2.py
@@ -0,0 +1,7 @@
+# This is just a comment. 
+# Don't expect much from this, unless you want a cookie.
+
+
+print "I wish that there would be no more diseases, war or famine." # Ignore this statement.
+
+print "Don't you?" # Ignore this statement again.
