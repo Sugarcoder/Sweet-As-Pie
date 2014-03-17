73471a2d18bcfae11b686d1a5a6f02c6d7968341
Code Honey <codehoney@users.noreply.github.com>
2014-03-17 10:20:35 -0700

    Create ex4.py
    Eating brownies for dessert.

---
+++ ex4.py
@@ -0,0 +1,9 @@
+brownies = 10
+people = 5
+number = 2
+
+
+
+print "There are", brownies, "brownies available for dessert."
+print "There are", people, "people at the dinner party."
+print "Each person will eat", number, "brownies."
