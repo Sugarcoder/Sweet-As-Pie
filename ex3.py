218d75a328472bb101ed83d114410fb673f8ca69
Code Honey <codehoney@users.noreply.github.com>
2014-03-17 10:08:21 -0700

    Create ex3.py
    Who doesn't like math?

---
+++ ex3.py
@@ -0,0 +1,20 @@
+print "I will now count my friends."
+
+print "Lads", 10 + 5 / 3
+print "Ladies", 12 + 8 / 4
+
+print "Now I will count the food"
+
+print 1 + 1 + 2 + 2 + 3 + 3 / 3
+
+print "Is it true that 12 / 3 > 12 / 4?"
+print 12 / 3 > 12 / 4
+
+print "What is 12 / 3?", 12 / 3 
+print "What is 12 / 4?", 12 / 4 
+
+print "Oh, that's why its True."
+
+print "Is it greater?", 5 > 3
+print "Is it greater or equal to?", 5 >= 3
+print "Is it less than?", 5 < 3
